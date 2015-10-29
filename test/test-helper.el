e3e6b0caf3dcd228aa3ec3640ce63d960c36f795
Victor Deryagin <vderyagin@gmail.com>
2015-10-29 19:00:53 +0200

    "with-org" macro: fail when point moves

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,10 +1,17 @@
 (require 'orgtd)
+(require 'cl-macs)
 
 (defmacro with-org (text &rest body)
   (declare (indent 1))
-  `(with-temp-buffer
-     (org-mode)
-     (insert ,text)
-     (goto-char (point-min))
-     (search-forward "<POINT>" nil t)
-     ,@body))
+  (let ((position (cl-gensym)))
+    `(with-temp-buffer
+       (org-mode)
+       (insert ,text)
+       (goto-char (point-min))
+       (search-forward "<POINT>" nil t)
+       (let ((,position (point)))
+         ,@body
+         (unless (= ,position (point))
+           (error "Point moved from %d to %d when it should not have"
+                  ,position
+                  (point)))))))
