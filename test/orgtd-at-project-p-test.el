cd402c194eb53b515566beead0c405f9ee9ca908
Victor Deryagin <vderyagin@gmail.com>
2019-07-26 22:44:59 +0300

    Move one test file to buttercup

--- test/orgtd-at-project-p-test.el
+++ test/orgtd-at-project-p-test.el
@@ -1,45 +1,46 @@
-(ert-deftest rejects-non-project-headings ()
-  (with-org "* plain heading"
-    (should-not (orgtd-at-project-p))))
+(describe "orgtd-atp-roject-p"
+  (it "rejects non-project headings"
+    (with-org "* plain heading"
+      (expect (orgtd-at-project-p) :to-be nil)))
 
-(ert-deftest accepts-empty-todo-item-with-set-property ()
-  (let ((orgtd-project-property-name "IS_PROJECT"))
-    (with-org "* TODO empty project heading
+  (it "accepts empty todo item with IS_PROJECT property set"
+    (let ((orgtd-project-property-name "IS_PROJECT"))
+      (with-org "* TODO empty project heading
 :PROPERTIES:
 :IS_PROJECT: true
 :END:
 "
-      (should (orgtd-at-project-p)))))
+        (expect (orgtd-at-project-p) :to-be-truthy))))
 
-(ert-deftest accepts-empty-todo-item-with-project-latest-activity-timestamp ()
-  (let ((orgtd-project-latest-activity-property-name "LAST_ACTIVE"))
-    (with-org "* TODO empty project heading
+  (it "accepts empty todo item with project LAST_ACTIVE property"
+    (let ((orgtd-project-latest-activity-property-name "LAST_ACTIVE"))
+      (with-org "* TODO empty project heading
 :PROPERTIES:
 :LAST_ACTIVE: [2016-06-27 Mon 13:29]
 :END:
 "
-      (should (orgtd-at-project-p)))))
+        (expect (orgtd-at-project-p) :to-be-truthy))))
 
-(ert-deftest rejects-empty-todo-items-plain-headings ()
-  (with-org "* TODO plain todo item"
-    (should-not (orgtd-at-project-p))))
+  (it "rejects empty todo items with plain headings"
+    (with-org "* TODO plain todo item"
+      (expect (orgtd-at-project-p) :to-be nil)))
 
-(ert-deftest accepts-todo-item-with-immediately-nested-todo-item ()
-  (with-org "* TODO project
+  (it "accepts todo item with immediately nested todo item"
+    (with-org "* TODO project
 ** TODO task"
-    (should (orgtd-at-project-p))))
+      (expect (orgtd-at-project-p) :to-be-truthy)))
 
-(ert-deftest accepts-todo-item-with-deeply-nested-todo-item ()
-  (with-org "* TODO foo
+  (it "accepts todo item with deeply nested todo item"
+    (with-org "* TODO foo
 ** bar
 *** baz
 **** quux
 ***** corge
 ****** TODO finally another todo"
-    (should (orgtd-at-project-p))))
+      (expect (orgtd-at-project-p) :to-be-truthy)))
 
-(ert-deftest rejects-any-heading-if-it-is-under-todo-item ()
-  (with-org "* TODO top-level-todo
+  (it "rejects any heading if it is under todo item"
+    (with-org "* TODO top-level-todo
 ** TODO <POINT>second-level-todo
 *** TODO third-level todo"
-    (should-not (orgtd-at-project-p))))
+      (expect (orgtd-at-project-p) :to-be nil))))
