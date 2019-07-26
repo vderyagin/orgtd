6183f77ab01ba78026866a5595f79e99fb789144
Victor Deryagin <vderyagin@gmail.com>
2019-07-26 22:57:01 +0300

    fix test names

--- test/orgtd-contains-next-p-test.el
+++ test/orgtd-contains-next-p-test.el
@@ -1,30 +1,30 @@
 (describe "orgtd-contains-next-p"
-  (it "rejects-headings-withoug-todo-keywords"
+  (it "rejects headings without todo keywords"
     (with-org "* plain heading"
       (expect (orgtd-contains-next-p) :to-be nil)))
 
-  (it "rejects-bare-next-items"
+  (it "rejects bare NEXT items"
     (with-org "* NEXT foo"
       (expect (orgtd-contains-next-p) :to-be nil)))
 
-  (it "accepts-heading-with-nested-next-item"
+  (it "accepts heading with nested NEXT item"
     (with-org "* plain heading
 ** NEXT next thing"
       (expect (orgtd-contains-next-p) :to-be-truthy)))
 
-  (it "accepts-heading-with-deeply-nested-next-item"
+  (it "accepts heading with deeply nested NEXT item"
     (with-org "* TODO project
 ** TODO subproject
 *** TODO subsubproject
 **** NEXT next task"
       (expect (orgtd-contains-next-p) :to-be-truthy)))
 
-  (it "is-not-fooled-by-non-heading-containing-NEXT"
+  (it "is not fooled by non-heading containing NEXT keyword"
     (with-org "* heading
 foo bar * NEXT baz")
     (expect (orgtd-contains-next-p) :to-be nil))
 
-  (it "accepts-other-keywords-if-set"
+  (it "accepts other keywords if set"
     (with-org "* TODO project
 ** TODO subproject
 *** TODO subsubproject
