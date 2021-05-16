a7aa40246da4fa6217626194b2ccf9be76fab27a
Victor Deryagin <vderyagin@gmail.com>
2021-05-16 15:10:30 +0300

    v0.2.6

--- orgtd.el
+++ orgtd.el
@@ -5,7 +5,7 @@
 ;; Author: Victor Deryagin <vderyagin@gmail.com>
 ;; Maintainer: Victor Deryagin <vderyagin@gmail.com>
 ;; Created: 22 Oct 2015
-;; Version: 0.2.5
+;; Version: 0.2.6
 
 ;; Package-Requires: ((helm))
 
