2533c7d74270ff5c844e4bd7b158f2ce7d8b26d7
Victor Deryagin <vderyagin@gmail.com>
2026-01-06 23:30:32 +0200

    version  now updated

--- orgtd.el
+++ orgtd.el
@@ -5,7 +5,7 @@
 ;; Author: Victor Deryagin <vderyagin@gmail.com>
 ;; Maintainer: Victor Deryagin <vderyagin@gmail.com>
 ;; Created: 22 Oct 2015
-;; Version: 0.4.0
+;; Version: 0.5.0
 
 ;; Package-Requires: ((consult) (embark) (embark-consult))
 
